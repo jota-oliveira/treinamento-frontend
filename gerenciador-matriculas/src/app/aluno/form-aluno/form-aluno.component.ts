3afc3dca8d859ec26ced21aae88264e8774ffcea
João Henrique de Oliveira Júnior <joao.holiveira@totvs.com.br>
2020-02-21 17:08:17 -0300

    Permitindo formulário de aluno autopreencher no setup inicial

--- gerenciador-matriculas/src/app/aluno/form-aluno/form-aluno.component.ts
+++ gerenciador-matriculas/src/app/aluno/form-aluno/form-aluno.component.ts
@@ -3,6 +3,9 @@ import {
   OnInit,
   Output,
   EventEmitter,
+  Input,
+  OnChanges,
+  SimpleChanges,
 } from '@angular/core';
 import { Aluno } from 'aluno/entities/aluno';
 import { ValidaCPF } from 'utils/form-validators-customizados';
@@ -13,23 +16,37 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   templateUrl: './form-aluno.component.html',
   styleUrls: ['./form-aluno.component.css']
 })
-export class FormAlunoComponent implements OnInit {
+export class FormAlunoComponent implements OnInit, OnChanges {
 
   public formAluno: FormGroup;
+  private dadosIniciais: Aluno = null;
 
+  @Input() aluno: Aluno;
   @Output() salvarAluno: EventEmitter<any> = new EventEmitter();
 
   constructor(private formBuilder: FormBuilder) { }
 
+  ngOnChanges(changes: SimpleChanges) {
+    this.dadosIniciais = changes.aluno.currentValue;
+  }
+
   ngOnInit() {
-    this.criarFormulario(new Aluno({
+    this.criarFormulario(
+      this.buscarDadosIniciaisDoFormulario()
+    );
+  }
+
+  private buscarDadosIniciaisDoFormulario(): Aluno {
+    if (this.dadosIniciais) { return this.dadosIniciais; }
+
+    return new Aluno({
       nome: '',
       email: '',
       cpf: null,
       matricula: null,
       formaIngresso: '',
       turma: []
-    }));
+    });
   }
 
   private criarFormulario(aluno: Aluno) {
